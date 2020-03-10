ecde47b82e41b84548dadb43f36f4ae06b189bf8
Matt Maas <matt.maas@radpartners.com>
2020-03-10 15:20:44 -0400

    created vpc main.tf

---
+++ vpc/main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_vpc" "dev-vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name = "dev-vpc"
+  }
+}
+
+resource "aws_subnet" "dev-subnet" {
+  vpc_id                  = aws_vpc.dev-vpc.id
+  availability_zone       = "us-east-2a"
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = "true"
+  tags = {
+    Name = "dev-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "dev-igw" {
+  vpc_id = aws_vpc.dev-vpc.id
+  tags = {
+    Name = "dev-igw"
+  }
+}
+
+resource "aws_route_table" "dev-public-rt" {
+  vpc_id = aws_vpc.dev-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dev-igw.id
+
+  }
+
+  tags = {
+    Name = "dev-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "dev-rta-public-subnet1" {
+  subnet_id = aws_subnet.dev-subnet.id
+  route_table_id = aws_route_table.dev-public-rt.id
+}
+
